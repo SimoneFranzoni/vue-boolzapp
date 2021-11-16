7327eaa26ae019e8ee1869b2703b521d08cac71e
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-16 10:04:56 +0100

    2

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+        el: '#app',
+        data:{
+            user: {
+                name: '',
+                avatar: 'img', 
+            },
+            contacts: [
+                {
+                    name: '',
+                    
+                }
+            ]
+        }
+
+
+})
